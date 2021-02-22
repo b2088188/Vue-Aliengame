1900c03dcd05b8259190b12b92ebb1bc3f446d0a
Shunze Lin <4A490101@stust.edu.tw>
2021-02-22 15:58:50 +0800

    feat: Adding Application UI state

---
+++ src/components/GamestateStart.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="modal">
+		<h1 id="header">You're an alienzombie.</h1>
+		<p>
+			Lorem, ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
+			aspernatur suscipit numquam quaerat iusto in, minima quod fugit
+			accusamus deserunt inventore dolorem, eaque quasi aperiam,
+			praesentium maxime error quos est.
+		</p>
+		<p>
+			They look suspicous. Answer the questions right to make sure ide
+			kept secret!
+		</p>
+		<slot></slot>
+	</div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
